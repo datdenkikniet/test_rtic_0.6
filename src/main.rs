3cbe7021cc13ee8d60a83738ce7d49fb58ecdb24
Johannes Draaijer <johannes@widefind.se>
2021-05-12 13:21:57 +0200

    Low power timer minimal example

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,23 @@ mod app {
     use rtic::time::duration::Seconds;
     use rtic::Monotonic;
     use rtt_target::{rprintln, rtt_init_print};
-    use stm32l4xx_hal::pac::TIM2;
-    use stm32l4xx_hal::timer::Timer;
+    use stm32l4xx_hal::lptimer::{
+        ClockSource, LowPowerTimer, LowPowerTimerConfig, LowPowerTimerMonotonic,
+    };
     use stm32l4xx_hal::{
         prelude::*,
-        rcc::{ClockSecuritySystem, CrystalBypass, MsiFreq},
-        time::Hertz,
-        timer,
+        rcc::{ClockSecuritySystem, CrystalBypass},
     };
 
     #[monotonic(binds = TIM2, default = true)]
-    type MyMono2 = Timer<TIM2>;
+    type MyMono2 = LowPowerTimerMonotonic<32_768>;
 
     #[init]
     fn init(cx: init::Context) -> (init::LateResources, init::Monotonics) {
         let mut flash = cx.device.FLASH.constrain();
         let mut rcc = cx.device.RCC.constrain();
         let mut pwr = cx.device.PWR.constrain(&mut rcc.apb1r1);
+        let lptim = cx.device.LPTIM1;
 
         rtt_init_print!(NoBlockSkip, 4096);
 
@@ -36,24 +36,22 @@ mod app {
         let clocks = rcc
             .cfgr
             .sysclk(80.mhz())
+            .lse(CrystalBypass::Enable, ClockSecuritySystem::Disable)
             .freeze(&mut flash.acr, &mut pwr);
 
-        // let mut delay = DelayCM::new(clocks);
-
-        // Setup the monotonic timer
-        let mono = timer::Timer::free_running_tim2(
-            cx.device.TIM2,
-            clocks,
-            Hertz(80_000_000),
-            true,
+        let mono = LowPowerTimer::lptim1(
+            lptim,
+            LowPowerTimerConfig::default().clock_source(ClockSource::LSE),
             &mut rcc.apb1r1,
+            &mut rcc.ccipr,
+            clocks,
         );
 
         rprintln!("init");
 
         bar::spawn_after(Seconds(1u32)).ok();
 
-        (init::LateResources {}, init::Monotonics(mono))
+        (init::LateResources {}, init::Monotonics(mono.into()))
     }
 
     #[task]
